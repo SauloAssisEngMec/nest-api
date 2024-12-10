7a90ec9d8dd06734937b532318ad90203996a6df
saulo <sauloassis4123@gmail.com>
2024-12-10 17:50:16 -0300

    feat: Google MAps API integrated

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
+import { Client as GoogleMapsCleint } from '@googlemaps/google-maps-services-js';
 
 @Module({
   controllers: [PlacesController],
-  providers: [PlacesService]
+  providers: [
+    PlacesService,
+    {
+      provide: GoogleMapsCleint,
+      useValue: new GoogleMapsCleint({}),
+    },
+  ],
 })
 export class MapsModule {}
--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import {
+  Client as GoogleMapsCleint,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PlacesService {
-  findPlaces(text: string) {}
+  constructor(
+    private googleMapsClient: GoogleMapsCleint,
+    private configService: ConfigService,
+  ) {}
+  async findPlaces(text: string) {
+    const { data } = await this.googleMapsClient.findPlaceFromText({
+      params: {
+        input: text,
+        inputtype: PlaceInputType.textQuery,
+        fields: ['place_id', 'formatted_address', 'geometry', 'name'],
+        key: this.configService.get('GOOGLE_MAPS_API_KEY'),
+      },
+    });
+
+    return { data };
+  }
 }
