fffa8ba84f5d565f63a8a918cf481393e75c19a6
saulo <sauloassis4123@gmail.com>
2024-12-10 22:23:34 -0300

    feat: directions by origin and destination is integrated

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
 import { Client as GoogleMapsCleint } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
 
 @Module({
-  controllers: [PlacesController],
+  controllers: [PlacesController, DirectionsController],
   providers: [
     PlacesService,
     {
       provide: GoogleMapsCleint,
       useValue: new GoogleMapsCleint({}),
     },
+    DirectionsService,
   ],
 })
 export class MapsModule {}
